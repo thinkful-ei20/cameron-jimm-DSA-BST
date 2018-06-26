3b9469f36bf5185ca09422454c508c9e9b37ee62
unknown <ajameslusk@gmail.com>
2018-06-26 12:03:13 -0400

    created bst calss

---
+++ bst.js
@@ -0,0 +1,11 @@
+
+
+class BST {
+  constructor(key=null, value=null, parent=null){
+    this.key = key;
+    this.value = value;
+    this.parent = parent;
+    this.left = null;
+    this.right = null;
+  }
+}
