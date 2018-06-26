fbb5b8ea8e66001a3ca0788c866dbbe548b8cd1c
Cam Prier <CSPrier@gmail.com>
2018-06-26 12:19:59 -0700

    Clarified what insert method does

--- bst.js
+++ bst.js
@@ -9,6 +9,15 @@ class BST {
     this.right = null;
   }
 
+  // ---- INSERT ----------------------------------------
+  // Check if the key exists, if it doesnt, create a new leaf
+  // if the key is less than this.key, check if its left branch exists,
+  // if it doesn't, create a new branch
+  // if it does, insert recursively
+  // else
+  // if the key is less than this.key, check if its right branch exists,
+  // if it doesn't, create a new branch
+  // if it does, insert recursively
   insert(key, value) {
     // Check if the key is null, if it is, 
     // set this.key to key and this.value to value
