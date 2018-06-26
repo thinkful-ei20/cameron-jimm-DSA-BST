d93bb41dfc82fa7f40006c9fef2c4b130c72858b
Cam Prier <CSPrier@gmail.com>
2018-06-26 12:15:27 -0700

    Added insert method to BST class

--- bst.js
+++ bst.js
@@ -1,4 +1,4 @@
-
+'use strict';
 
 class BST {
   constructor(key=null, value=null, parent=null){
@@ -8,4 +8,36 @@ class BST {
     this.left = null;
     this.right = null;
   }
+
+  insert(key, value) {
+    // Check if the key is null, if it is, 
+    // set this.key to key and this.value to value
+    if (this.key === null) {
+      this.key = key;
+      this.value = value;
+    } 
+    else if (key < this.key) {
+      // if this.left doesnt exist
+      // set this.left to a new BST
+      if (this.left === null) {
+        this.left = new BST(key, value, this);
+      } else {
+        // else, if it does exist
+        // insert key, value on the left(recursive)
+        this.left.insert(key, value);
+      }
+    }
+    // Else 
+    else {
+      // check if this.right exists
+      if (this.right === null) { 
+        // if it doesn't, assign this.right to a new BST
+        this.right = new BST(key, value, this);
+      } else {
+        // otherwise, insert key, value on the right(recursive)
+        this.right.insert(key, value);
+      }
+    }
+  }
+
 }
