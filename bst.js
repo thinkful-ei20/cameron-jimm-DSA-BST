e46c51f7a749600c6836af1def9671d4b9698940
unknown <ajameslusk@gmail.com>
2018-06-26 14:15:55 -0400

    finished basic BST class

--- bst.js
+++ bst.js
@@ -19,34 +19,104 @@ class BST{
   // if it doesn't, create a new branch
   // if it does, insert recursively
   insert(key, value) {
-    // Check if the key is null, if it is, 
-    // set this.key to key and this.value to value
     if (this.key === null) {
       this.key = key;
       this.value = value;
     } 
     else if (key < this.key) {
-      // if this.left doesnt exist
-      // set this.left to a new BST
       if (this.left === null) {
         this.left = new BST(key, value, this);
       } else {
-        // else, if it does exist
-        // insert key, value on the left(recursive)
         this.left.insert(key, value);
       }
     }
-    // Else 
     else {
-      // check if this.right exists
       if (this.right === null) { 
-        // if it doesn't, assign this.right to a new BST
         this.right = new BST(key, value, this);
       } else {
-        // otherwise, insert key, value on the right(recursive)
         this.right.insert(key, value);
       }
     }
+  }
+
+  find(key) {
+    if (this.key === key) {
+      return this.value;
+    }
+    else if (key < this.key && this.left) {
+      return this.left.find(key);
+    }
+    else if (key > this.key && this.right) {
+      return this.right.find(key);
+    }
+    else {
+      throw new Error('Key Error');
+    }
+  }
+
+  remove(key) {
+    if (this.key === key) {
+      if (this.left && this.right) {
+        const successor = this.right._findMin();
+        this.key = successor.key;
+        this.value = successor.value;
+        successor.remove(successor.key);
+      }  
+      else if (this.left) {
+        this._replaceWith(this.left);
+      }
+      else if (this.right) {
+        this._replaceWith(this.right);
+      }
+      else {
+        this._replaceWith(null);
+      }
+    }
+    else if (key < this.key && this.left) {
+      this.left.remove(key);
+    }
+    else if (key > this.key && this.right) {
+      this.right.remove(key);
+    }
+    else {
+      throw new Error('Key Error');
+    }
+  }
+
+  _replaceWith(node) {
+    if (this.parent) {
+      if (this === this.parent.left) {
+        this.parent.left = node;
+      }
+      else if (this === this.parent.right) {
+        this.parent.right = node;
+      }
+
+      if (node) {
+        node.parent = this.parent;
+      }
+    }
+    else {
+      if (node) {
+        this.key = node.key;
+        this.value = node.value;
+        this.left = node.left;
+        this.right = node.right;
+      }
+      else {
+        this.key = null;
+        this.value = null;
+        this.left = null;
+        this.right = null;
+      }
+    }
+  }
+
+  _findMin() {
+    if (!this.left) {
+      return this;
+    }
+    return this.left._findMin();
   }
 
 }
